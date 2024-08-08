802061ebc029a9fa4e236dd42c7dcbb87945dcdb
Chirag Tayal <chirag@Chirags-MacBook-Air.local>
2024-08-08 17:56:04 +0530

    Tf Final#2

--- variables.tf
+++ variables.tf
@@ -1,10 +1,3 @@
-variable "prefix" {
-  description = "Prefix for the resources"
-  type        = string
-  default     = "react"
-
-}
-
 variable "location" {
   description = "Location for the resources"
   type        = string
@@ -18,28 +11,38 @@ variable "app" {
 }
 
 variable "azure_devops_pat" {
+  description = "Azure DevOps Personal Access Token"
+  type        = string
+  default     = "cjhwmmmwwxhzupfgbwuysz5te23rf2xp66wsh6773rh7yqdryuaq"
 
 }
 variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
 }
 
 variable "subscription_id" {
- 
+  description = "Azure Subscription ID"
+  type        = string
 }
 
 variable "subscription_name" {
   description = "Azure Subscription Name"
   type        = string
-  default = "Azure for Students"
+  default     = "Azure for Students"
 }
 
 variable "client_id" {
+  description = "Azure Client ID"
+  type        = string
 }
 
 variable "client_secret" {
-
+  description = "Azure Client Secret"
+  type        = string
 }
 
 variable "prefix" {
-
+  description = "Prefix for resource names"
+  type        = string
 }
