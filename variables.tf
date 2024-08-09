67efe8fde4c565350b4c1fcc4afbab3167229c60
Chirag Tayal <chirag@Chirags-MacBook-Air.local>
2024-08-09 22:36:23 +0530

    Release

--- variables.tf
+++ variables.tf
@@ -1,61 +1,15 @@
-variable "location" {
+variable "location" {}
 
-}
+variable "app" {}
 
-variable "app" {
+variable "prefix" {}
 
-}
+variable "azure_devops_pat" {}
 
-variable "prefix" {
+variable "azure_tenant_id" {}
 
-}
+variable "azure_subscription_id" {}
 
-variable "azure_devops_pat" {
+variable "azure_service_principal_id" {}
 
-}
-
-variable "azure_tenant_id" {
-
-}
-
-variable "azure_subscription_id" {
-
-}
-
-variable "azure_service_principal_id" {
-
-}
-
-variable "azure_service_principal_key" {
-
-}
-
-# {
-#   "appId": "a4e32b68-8d2d-4d62-a890-21c4b9989e71",
-#   "displayName": "azure-cli-2024-08-08-14-47-07",
-#   "password": "GxW8Q~xCNHGr8aGFf6Na_h8is8k-CIqrnbigvbOL",
-#   "tenant": "625c37b3-4f97-45bb-b1a4-c080858632ab"
-# }
-
-# [
-#   {
-#     "cloudName": "AzureCloud",
-#     "homeTenantId": "625c37b3-4f97-45bb-b1a4-c080858632ab",
-#     "id": "17bd46f2-ce41-4bdf-b47b-2347dc85340d",
-#     "isDefault": true,
-#     "managedByTenants": [],
-#     "name": "Azure for Students",
-#     "state": "Enabled",
-#     "tenantId": "625c37b3-4f97-45bb-b1a4-c080858632ab",
-#     "user": {
-#       "name": "a4e32b68-8d2d-4d62-a890-21c4b9989e71",
-#       "type": "servicePrincipal"
-#     }
-#   }
-# ]
-
-# sh
-# export ARM_CLIENT_ID="a4e32b68-8d2d-4d62-a890-21c4b9989e71"
-# export ARM_CLIENT_SECRET="GxW8Q~xCNHGr8aGFf6Na_h8is8k-CIqrnbigvbOL"
-# export ARM_TENANT_ID="625c37b3-4f97-45bb-b1a4-c080858632ab"
-# export ARM_SUBSCRIPTION_ID="17bd46f2-ce41-4bdf-b47b-2347dc85340d"
+variable "azure_service_principal_key" {}
